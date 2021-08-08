3bb5dfee67d37233d8cd159bfaeef12f90cadf49
Suzan Masite <suzanm@gerbertech.co.za>
2021-08-08 12:38:42 +0200

    Added Models

---
+++ RouteImport/RouteImport/Models/Credentials.cs
@@ -0,0 +1,11 @@
+﻿namespace RouteImport.Models
+{
+    public class Credentials
+    {
+        public string Username { get; set; }
+        public string Server { get; set; }
+        public string SessionId { get; set; }
+        public string Database { get; set; }
+        public string groupFilter { get; set; }
+    }
+}
---
+++ RouteImport/RouteImport/Models/FileDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace RouteImport.Models
+{
+    public class FileDetails
+    {
+        public string Registration { get; set; }
+        public string CustomerCode { get; set; }
+        public int PickingSlip { get; set; }
+        public string StopSequence { get; set; }
+    }
+}
